702ffba3e0026b83a24b96ac9a612b694307c875
Tomasz Pielecki <tomasz.pielecki86@gmail.com>
2022-06-25 22:11:12 +0200

    String/Switch

---
+++ Day1/firstDay.py
@@ -0,0 +1,16 @@
+print("Welcome to the Band Name Generator.")
+city = (input("What's name of the city you grew up in?\n"))
+petName = input("Whar's your pet's name?\n")
+print(f"Your band name could be {city},{petName}")
+
+# String manipulation
+
+print("Tomasz is the best programer\nYes Tomasz is  great!!!\nI Know\n")
+print("Tomasz " + "Best of you")
+firstWord = "Tomasz"
+secondWord = "Master"
+print("old " + firstWord + " " + secondWord)
+print(f"May name is {firstWord} good programer in python have rang {secondWord}")
+print("Hello " + input("What your name?\n"))
+print(len(petName))
+print(len(input("What is your Car?\n")))
